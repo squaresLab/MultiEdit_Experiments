24ab30af7559102211d9d365a87e56814999c39f
zhenyudg <8439020+zhenyudg@users.noreply.github.com>
2020-02-11 14:50:29 -0500

    refactor test resources

--- DependencyAnalyzer/test/TestHashMutablePDG.java
+++ DependencyAnalyzer/test/TestHashMutablePDG.java
@@ -15,14 +15,14 @@ public class TestHashMutablePDG
 	@Test
 	public void testControlDependency()
 	{
-		String[] args = {"test-resources", "org.apache.commons.math3.util.ContinuedFraction", "000", "170"};
+		String[] args = {"test-resources/Math31b", "org.apache.commons.math3.util.ContinuedFraction", "000", "170"};
 		Main.main(args);
 	}
 
 	@Test
 	public void testFlowDependency()
 	{
-		String[] args = {"test-resources", "org.apache.commons.math3.util.ContinuedFraction", "100", "170"};
+		String[] args = {"test-resources/Math31b", "org.apache.commons.math3.util.ContinuedFraction", "100", "170"};
 		Main.main(args);
 	}
 }
