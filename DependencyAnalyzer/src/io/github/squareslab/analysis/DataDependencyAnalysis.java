0da36f13231005ac07b2d276e7786e4064a52869
zhenyudg <8439020+zhenyudg@users.noreply.github.com>
2020-02-11 11:31:19 -0500

    Add lines->units mapper.

--- DependencyAnalyzer/src/io/github/squareslab/analysis/DataDependencyAnalysis.java
+++ DependencyAnalyzer/src/io/github/squareslab/analysis/DataDependencyAnalysis.java
@@ -20,6 +20,29 @@ public class DataDependencyAnalysis extends BodyTransformer
 		this.lineNumsOfInterest = lineNumbersOfInterest;
 	}
 
+	private Map<Integer, Collection<Unit>> getLinesToUnitsMap(Iterable<Unit> units)
+	{
+		Map<Integer, Collection<Unit>> map = new HashMap<>();
+
+		for (Unit unit : units)
+		{
+			int lineNum = unit.getJavaSourceStartLineNumber();
+
+			if (lineNum == -1)
+				continue;
+
+			//if lineNum is previously unseen, add an empty value set to map
+			if (! map.containsKey(lineNum))
+			{
+				Collection<Unit> unitsForLineNum = new ArrayList<>();
+				map.put(lineNum, unitsForLineNum);
+			}
+
+			map.get(lineNum).add(unit);
+		}
+
+		return map;
+	}
 
 
 	/**
