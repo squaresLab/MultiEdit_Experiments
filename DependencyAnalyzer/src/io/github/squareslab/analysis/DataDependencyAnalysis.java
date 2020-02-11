49e8f1fa4be25e5b401bb62c0a75edab1d6cff25
zhenyudg <8439020+zhenyudg@users.noreply.github.com>
2020-02-10 22:35:21 -0500

    parse the graph for in/out sets rather than do dataflow analysis

--- DependencyAnalyzer/src/io/github/squareslab/analysis/DataDependencyAnalysis.java
+++ DependencyAnalyzer/src/io/github/squareslab/analysis/DataDependencyAnalysis.java
@@ -4,7 +4,6 @@ import soot.*;
 import soot.jimple.*;
 import soot.toolkits.graph.ExceptionalUnitGraph;
 import soot.toolkits.graph.UnitGraph;
-import soot.toolkits.scalar.BackwardFlowAnalysis;
 
 import java.util.*;
 
@@ -21,11 +20,136 @@ public class DataDependencyAnalysis extends BodyTransformer
 		this.lineNumsOfInterest = lineNumbersOfInterest;
 	}
 
+	private boolean isGetter(Value value)
+	{
+		if (value instanceof InvokeExpr)
+		{
+			SootMethod invokedMethod = ((InvokeExpr) value).getMethod();
+			String methodName = invokedMethod.getName();
+			return methodName.startsWith("get");
+		}
+		else
+			return false;
+	}
+
+	private boolean isSetter(Unit unit)
+	{
+		if (unit instanceof InvokeStmt)
+		{
+			InvokeExpr invokeExpr = ((InvokeStmt) unit).getInvokeExpr();
+			SootMethod invokedMethod = invokeExpr.getMethod();
+			String methodName = invokedMethod.getName();
+			return methodName.startsWith("set");
+		}
+		else
+			return false;
+	}
+
+	private String getGetterSetterHeuristicName(InvokeExpr invokeExpr)
+	{
+		SootMethod invokedMethod = invokeExpr.getMethod();
+
+		String className = invokedMethod.getDeclaringClass().getName();
+
+		String methodName = invokedMethod.getName();
+		assert methodName.length() >= 3;
+		String attributeName = methodName.substring(3);
+
+		return className + "." + attributeName;
+	}
+
+	/**
+	 * Adds values contained in value to reads
+	 * @param value
+	 * @param reads
+	 */
+	private void addUsedValuesToReadsSet(Value value, Set<Object> reads)
+	{
+		List<ValueBox> usedValueBoxes = value.getUseBoxes();
+		for(ValueBox valueBox : usedValueBoxes)
+		{
+			Value usedValue = valueBox.getValue();
+
+			if (isGetter(usedValue))
+			{
+				String heuristicName = getGetterSetterHeuristicName((InvokeExpr) usedValue);
+				reads.add(heuristicName);
+			}
+			//we don't track constants
+			else if( ! (usedValue instanceof Constant))
+			{
+				reads.add(usedValue);
+			}
+		}
+	}
+
+	/**
+	 *
+	 * @param unit unit
+	 * @return the read and write sets of the unit
+	 */
+	private ReadWriteSets<Object> getReadWriteSets(Unit unit)
+	{
+		Set<Object> reads = new HashSet<>();
+		Set<Object> writes = new HashSet<>();
+
+		if (isSetter(unit))
+		{
+			InvokeExpr invokeExpr = ((InvokeStmt) unit).getInvokeExpr();
+			String heuristicNameOfSetVar = getGetterSetterHeuristicName(invokeExpr);
+			writes.add(heuristicNameOfSetVar);
+		}
+		else if (unit instanceof AssignStmt)
+		{
+			AssignStmt assignStmt = (AssignStmt) unit;
+
+			Value leftHandSide = assignStmt.getLeftOp();
+			writes.add(leftHandSide);
+
+			Value rightHandSide = assignStmt.getRightOp();
+			addUsedValuesToReadsSet(rightHandSide, reads);
+		}
+		else if (unit instanceof IfStmt)
+		{
+			Value ifCondition = ((IfStmt) unit).getCondition();
+			addUsedValuesToReadsSet(ifCondition, reads);
+		}
+		else if (unit instanceof SwitchStmt)
+		{
+			Value switchKey = ((SwitchStmt) unit).getKey();
+			addUsedValuesToReadsSet(switchKey, reads);
+
+			//switch cases in Java must be constants; therefore, we don't analyze them.
+		}
+		else if (unit instanceof ReturnStmt)
+		{
+			//treat returns as a special case of assignment where we don't know the LHS
+			Value returnValue = ((ReturnStmt) unit).getOp();
+			addUsedValuesToReadsSet(returnValue, reads);
+		}
+
+		return new ReadWriteSets<>(reads, writes);
+	}
+
+	private Map<Unit, ReadWriteSets<Object>> getReadWriteSets(Iterable<Unit> units)
+	{
+		Map<Unit, ReadWriteSets<Object>> map = new HashMap<>();
+		for(Unit unit : units)
+		{
+			map.put(unit, getReadWriteSets(unit));
+		}
+
+		return map;
+	}
+
 	@Override
 	protected void internalTransform(Body body, String phaseName, Map<String, String> options)
 	{
 		UnitGraph graph = new ExceptionalUnitGraph(body);
 
+		Map<Unit, ReadWriteSets<Object>> readWriteMap = getReadWriteSets(graph);
+
+		return;
 	}
 }
 
--- DependencyAnalyzer/test/TestHashMutablePDG.java
+++ DependencyAnalyzer/test/TestHashMutablePDG.java
@@ -1,5 +1,14 @@
 import io.github.squareslab.Main;
+import io.github.squareslab.analysis.ControlDependencyAnalysis;
+import io.github.squareslab.analysis.DataDependencyAnalysis;
+import io.github.squareslab.common.Utils;
 import org.junit.Test;
+import soot.PackManager;
+import soot.Transform;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 
 public class TestHashMutablePDG
 {
@@ -8,5 +17,37 @@ public class TestHashMutablePDG
 	{
 		String[] args = {"test-resources", "org.apache.commons.math3.util.ContinuedFraction", "170"};
 		Main.main(args);
+	}
+
+	@Test
+	public void testReadWriteAnalyzer()
+	{
+		String[] args = {"test-resources", "org.apache.commons.math3.util.ContinuedFraction", "170"};
+
+		String classpathToAnalysisTarget = args[0];
+		String classToAnalyze = args[1];
+		Collection<Integer> lineNumsOfInterest = parseIntArgs(args, 2, args.length);
+
+		PackManager.v().getPack("jap")
+				.add(new Transform(DataDependencyAnalysis.ANALYSIS_NAME, new DataDependencyAnalysis(lineNumsOfInterest)));
+		String[] sootArgs = Utils.getSootArgs(DataDependencyAnalysis.ANALYSIS_NAME, classpathToAnalysisTarget, classToAnalyze);
+		Utils.runSoot(sootArgs);
+	}
+
+	private static Collection<Integer> parseIntArgs(String[] args, int startIndex, int endIndex)
+	{
+		Collection<Integer> parseOutput = new ArrayList<>(endIndex - startIndex);
+		for(int i = startIndex; i < endIndex; i++)
+		{
+			try
+			{
+				int parsedInt = Integer.parseInt(args[i]);
+				parseOutput.add(parsedInt);
+			} catch (NumberFormatException e) {
+				System.err.printf("Skipping %s since it's not a valid line number", args[i]);
+			}
+		}
+
+		return parseOutput;
 	}
 }
