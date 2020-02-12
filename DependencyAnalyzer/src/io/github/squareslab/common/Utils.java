e140e6e016a7bcf2f42a91ddb0409bddee51ed24
zhenyudg <8439020+zhenyudg@users.noreply.github.com>
2020-02-12 14:52:11 -0500

    Eliminate unused warning/error reporting mechanism from 17-355 code

--- DependencyAnalyzer/src/io/github/squareslab/common/Utils.java
+++ DependencyAnalyzer/src/io/github/squareslab/common/Utils.java
@@ -3,14 +3,7 @@ package io.github.squareslab.common;
 import soot.Main;
 import soot.Body;
 import soot.Unit;
-import soot.SootMethod;
-import soot.SootField;
-import soot.SootClass;
-import soot.tagkit.Host;
 import soot.NormalUnitPrinter;
-
-import java.util.HashSet;
-import java.util.Set;
 
 import java.security.Permission;
 
@@ -24,29 +17,6 @@ public class Utils {
 		NormalUnitPrinter printer = new NormalUnitPrinter(body);
 		unit.toString(printer);
 		return printer.output().toString();
-	}
-
-	/** reports a warning message for the given element of source code,
-	 * using the ErrorMessage passed in.  The ErrorMessage, the name of
-	 * the element if it is a declaration, and the line number
-	 * are stored for later lookup.
-	 */
-	public static void reportWarning(Host element, ErrorMessage message) {
-		System.out.print("warning: ");
-		System.out.print(message.getErrorMessage());
-		int line = element.getJavaSourceStartLineNumber();
-		String name = getName(element);
-		if (line == -1) {
-			if (name != null) {
-				System.out.println(" at the declaration of " + name);
-			} else {
-				System.out.println(" (line unknown)");
-			}
-		} else {
-			System.out.print(" at line ");
-			System.out.println(line);
-		}
-		errors.add(new ErrorReport(message, line, name));
 	}
 
 	/** Builds up the appropriate arguments for invoking analysisToRun
@@ -76,34 +46,6 @@ public class Utils {
 			// swallow the exception if Soot tried to exit directly; we'll exit soon anyway
 		}
 	}
-
-	/** Resets the set of errors to be empty */
-	public static void resetErrors() {
-		errors = new HashSet<ErrorReport>();
-	}
-
-	/** Gets the set of all reported errors */
-	public static Set<ErrorReport> getErrors() {
-		return errors;
-	}
-
-	/** Returns the name of the element,
-	 * or null if the element is not a declaration */
-	public static String getName(Host element) {
-		// wow, wouldn't it be nice if there were a Declaration interface
-		// with the member getName()?
-		if (element instanceof SootClass) {
-			return ((SootClass)element).getName();
-		} else if (element instanceof SootField) {
-			return ((SootField)element).getName();
-		} else if (element instanceof SootMethod) {
-			return ((SootMethod)element).getName();
-		} else {
-			return null;
-		}
-	}
-
-	private static HashSet<ErrorReport> errors = new HashSet<ErrorReport>();
 
 	private static class ExitTrappedException extends SecurityException { }
 
