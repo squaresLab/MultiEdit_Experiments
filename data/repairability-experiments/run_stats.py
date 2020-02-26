221ae63da057be17441c646de7b3f1d23b93efd8
zhenyudg <8439020+zhenyudg@users.noreply.github.com>
2020-02-26 14:17:44 -0500

    separate multi-edit bugs into d4j and bears

--- data/repairability-experiments/run_stats.py
+++ data/repairability-experiments/run_stats.py
@@ -5,16 +5,19 @@ def get_multi_edit_bugs():
     with open('patch_locs.json') as f:
         patch_locs_json = json.load(f)
 
-    multi_edit_bugs = list()
+    multi_edit_bugs_d4j, multi_edit_bugs_bears = list(), list()
 
     for entry in patch_locs_json:
-        bugId = entry['bugId'].replace(' ', '') #Remove the space in D4J bugIds
+        bugId = str(entry['bugId']).replace(' ', '') #Remove the space in D4J bugIds
         patch = entry['patch']
         lines_edited = sum(len(edits) for edits in patch.values())
         if lines_edited > 1:
-            multi_edit_bugs.append(bugId)
+            if bugId[:5] == 'Bears':
+                multi_edit_bugs_bears.append(bugId)
+            else:
+                multi_edit_bugs_d4j.append(bugId)
 
-    return multi_edit_bugs
+    return multi_edit_bugs_d4j, multi_edit_bugs_bears
 
 def get_dependencies():
     dependencies = dict() #maps bugId -> 6-Tuple info on dependencies
@@ -29,7 +32,8 @@ def get_dependencies():
     return dependencies
 
 if __name__=='__main__':
-    multi_edit_bugs = get_multi_edit_bugs() #collection of multi-edit bugs
+    multi_edit_bugs_d4j, multi_edit_bugs_bears = get_multi_edit_bugs() #collection of multi-edit bugs
     dependencies = get_dependencies() #maps bugId -> 6-Tuple info on dependencies
+    print(multi_edit_bugs_bears)
 
 #todo: get info on which bugs are repairable by which techniques
