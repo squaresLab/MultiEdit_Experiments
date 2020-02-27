b1f36b8dd83be89429398e4d67343969feec7c8e
Serena Chen <serenazychen@gmail.com>
2020-02-27 17:08:20 -0500

    getting lists of symptoms; wrote up a symptoms section in the paper

--- src/main/python/classifications/smaller_classify.py
+++ src/main/python/classifications/smaller_classify.py
@@ -29,14 +29,21 @@ def is_date(s):
 
 
 """precondition: err + msg does actually reflect some kind of assertion"""
-def classify_assertion(err, msg_lower, error_dict, row_dict):
+def classify_assertion(err, msg_lower, error_dict=None, row_dict=None):
+	if not error_dict:
+		error_dict = defaultdict(int)
+	if not row_dict:
+		row_dict = defaultdict(bool)
+
 	if msg_lower == "":
-		row_dict["other_assert"] = True 
+		row_dict["other_assert"] = True
 		error_dict["other_assert"] += 1
+		return "other_assert" 
 	else:
 		if "null" in msg_lower:
 			row_dict["assert_null"] = True
 			error_dict["assert_null"] += 1
+			return "assert_null"
 		else:
 			common_pattern = re.compile(".*expected: ?[\[<]?(?P<e>.+?)[]>]? but was: ?[\[<]?(?P<a>.+?)[]>]?")
 			match = common_pattern.fullmatch(msg_lower)
@@ -44,35 +51,48 @@ def classify_assertion(err, msg_lower, error_dict, row_dict):
 				expected = match.group('e')
 				actual = match.group('a')
 				if is_int(expected) or is_float(expected) or expected == "true" or expected == "false":
-						row_dict["assert_prim"] = True 
+						row_dict["assert_prim"] = True
 						error_dict["assert_prim"] += 1
+						return "assert_prim" 
 
 				else:
-					row_dict["other_assert"] = True 
+					row_dict["other_assert"] = True
+					return "other_assert" 
 					error_dict["other_assert"] += 1
 					assertions[err].append(msg_lower)
 			else:
 				assertions[err].append(msg_lower)
-				row_dict["other_assert"] = True 
+				row_dict["other_assert"] = True
 				error_dict["other_assert"] += 1
+				return "other_assert" 
 
-def classify(err, msg, error_dict, row_dict):
+def classify(err, msg, error_dict=None, row_dict=None):
+	if not error_dict:
+		error_dict = defaultdict(int)
+	if not row_dict:
+		row_dict = defaultdict(bool)
+
 	msg_lower = msg.lower()
 	if err in timeout or "timeout" in msg_lower or "timed out" in msg_lower or "time out" in msg_lower:
 		error_dict["timeout"] += 1
 		row_dict["timeout"] = True
+		return "timeout"
 	elif err in test_assertions or "expected" in msg_lower:
 		# assertions[err].append(msg_lower)
-		classify_assertion(err, msg_lower, error_dict, row_dict)
+		return classify_assertion(err, msg_lower, error_dict, row_dict)
 	elif err in null_pointer:
 		error_dict["null_pointer"] += 1
 		row_dict["null_pointer"] = True
+		return "null_pointer"
 	elif err in parsing_conversion or err in serialization:
 		error_dict["parsing"] += 1
 		row_dict["parsing"] = True
+		return "parsing"
 	elif err in fields_arguments or err in instantiation_invocation_typing or err in index_out_of_bounds or err in not_found:
 		error_dict["access"] += 1
 		row_dict["access"] = True
+		return "access"
 	else:
 		error_dict["other"] += 1
 		row_dict["other"] = True
+		return "other"
--- src/main/python/classifications/smallerer_classify.py
+++ src/main/python/classifications/smallerer_classify.py
@@ -29,38 +29,55 @@ def is_date(s):
 
 
 """precondition: err + msg does actually reflect some kind of assertion"""
-def classify_assertion(err, msg_lower, error_dict, row_dict):
+def classify_assertion(err, msg_lower, error_dict=None, row_dict=None):
+	if not error_dict:
+		error_dict = defaultdict(int)
+	if not row_dict:
+		row_dict = defaultdict(bool)
+
 	if msg_lower == "":
-		row_dict["other_assert"] = True 
+		row_dict["other_assert"] = True
 		error_dict["other_assert"] += 1
+		return "other_assert" 
 	else:
 		common_pattern = re.compile(".*expected: ?[\[<]?(?P<e>.+?)[]>]? but was: ?[\[<]?(?P<a>.+?)[]>]?")
 		match = common_pattern.fullmatch(msg_lower)
 		if match:
 			expected = match.group('e')
 			actual = match.group('a')
-			row_dict["assert_equal"] = True 
+			row_dict["assert_equal"] = True
 			error_dict["assert_equal"] += 1
 			assertions[err].append(msg_lower)
+			return "assert_equal" 
 		else:
 			assertions[err].append(msg_lower)
-			row_dict["other_assert"] = True 
+			row_dict["other_assert"] = True
 			error_dict["other_assert"] += 1
+			return "other_assert" 
 
-def classify(err, msg, error_dict, row_dict):
+def classify(err, msg, error_dict=None, row_dict=None):
+	if not error_dict:
+		error_dict = defaultdict(int)
+	if not row_dict:
+		row_dict = defaultdict(bool)
+
 	msg_lower = msg.lower()
 	if err in test_assertions or "expected" in msg_lower:
 		# assertions[err].append(msg_lower)
-		classify_assertion(err, msg_lower, error_dict, row_dict)
+		return classify_assertion(err, msg_lower, error_dict, row_dict)
 	elif err in null_pointer:
 		error_dict["null_pointer"] += 1
 		row_dict["null_pointer"] = True
+		return "null_pointer"
 	elif err in parsing_conversion or err in serialization:
 		error_dict["parsing"] += 1
 		row_dict["parsing"] = True
+		return "parsing"
 	elif err in fields_arguments or err in instantiation_invocation_typing or err in index_out_of_bounds or err in not_found:
 		error_dict["access"] += 1
 		row_dict["access"] = True
+		return "access"
 	else:
 		error_dict["other"] += 1
 		row_dict["other"] = True
+		return "other"
