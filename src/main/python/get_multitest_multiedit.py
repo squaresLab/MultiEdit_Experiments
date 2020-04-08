40e95dced8e5c200e27b308095610a35291f335e
Serena Chen <serenazychen@gmail.com>
2020-04-08 16:54:53 -0400

    updated lists and a way to generate them

---
+++ src/main/python/get_multitest_multiedit.py
@@ -0,0 +1,42 @@
+import json
+
+with open("data/bears-bugs.json") as f:
+    bears_bugs = json.load(f)
+
+with open("data/defects4j-bugs.json") as f:
+    d4j_bugs = json.load(f)
+
+with open("data/multi_edit.txt") as f:
+    multi_edit = set([s.strip() for s in f])
+
+multi_test = []
+both = []
+
+for bug in d4j_bugs:
+    bugname = f'{bug["project"].upper()}:{int(bug["bugId"]):03}' #check if this is correct
+    tests = bug["failingTests"]
+    if len(tests) > 1:
+        multi_test.append(bugname)
+        if bugname in multi_edit:
+            both.append(bugname)
+
+for bug in bears_bugs:
+    bugname = f'BEARS:{int(bug["bugId"].split("-")[1]):03}' #check this is correct
+    tests = bug["tests"]["failureDetails"]
+    if len(tests) > 1:
+        multi_test.append(bugname)
+        if bugname in multi_edit:
+            both.append(bugname)
+
+print(multi_test)
+print(len(multi_test))
+print(both)
+print(len(both))
+
+with open("data/more_than_one_test.txt", "w") as f:
+    for b in multi_test:
+        f.write(b + "\n")
+
+with open("data/multitest_multiedit.txt", "w") as f:
+    for b in both:
+        f.write(b + "\n")
